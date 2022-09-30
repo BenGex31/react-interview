5ca7c7c3710513d87447f9c7dbc7b4ce62b21a81
Benjamin Desole <benjamin.desole@gmail.com>
2022-10-01 01:09:29 +0200

    Update App.tsx
    Display movie card with bold title and sub header category

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,35 @@
 import React, { useEffect, useState } from "react";
-import logo from "./logo.svg";
-import "./App.css";
 import { movies$ } from "./movies";
+import { Container } from "@mui/system";
+import { Grid } from "@mui/material";
+import MovieCard from "./components/MovieCard";
+
+export type movieObject = {
+  id: string;
+  title: string;
+  category: string;
+  likes: number;
+  dislikes: number;
+};
 
 function App() {
-  const [movies, setMovies] = useState(null);
+  const [movies, setMovies] = useState<movieObject[] | null>(null);
 
   useEffect(() => {
     movies$.then((value) => setMovies(value));
   }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth={"xl"}>
+      <Grid container justifyContent={"space-between"} spacing={2}>
+        {movies &&
+          movies.map((movie) => (
+            <Grid key={movie.id} item xs={12} sm={6} lg={4}>
+              <MovieCard title={movie.title} category={movie.category} />
+            </Grid>
+          ))}
+      </Grid>
+    </Container>
   );
 }
 
