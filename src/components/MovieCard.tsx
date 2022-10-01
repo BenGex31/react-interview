60cf8042704c0924e1fa19dd98c1befd1185ace3
Benjamin Desole <benjamin.desole@gmail.com>
2022-10-01 02:22:42 +0200

    Update MovieCard.tsx
    display likes and dislikes

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,12 +1,16 @@
-import { Card, CardHeader, Typography } from "@mui/material";
+import { ThumbDownAlt, ThumbUpAlt } from "@mui/icons-material";
+import { Card, CardHeader, Stack, Tooltip, Typography } from "@mui/material";
+import { green, red } from "@mui/material/colors";
 import React from "react";
 
 type movieCardProps = {
   title: string;
   category: string;
+  likes: number;
+  dislikes: number;
 };
 
-const MovieCard = ({ title, category }: movieCardProps) => {
+const MovieCard = ({ title, category, likes, dislikes }: movieCardProps) => {
   return (
     <Card>
       <CardHeader
@@ -16,6 +20,18 @@ const MovieCard = ({ title, category }: movieCardProps) => {
           </Typography>
         }
         subheader={category}
+        action={
+          <Stack direction={"row"} spacing={3}>
+            <Stack direction={"row"} spacing={1}>
+              <ThumbUpAlt sx={{ color: green[500] }} />{" "}
+              <Typography>{likes}</Typography>
+            </Stack>
+            <Stack direction={"row"} spacing={1}>
+              <ThumbDownAlt sx={{ color: red[500] }} />{" "}
+              <Typography>{dislikes}</Typography>
+            </Stack>
+          </Stack>
+        }
       />
     </Card>
   );
