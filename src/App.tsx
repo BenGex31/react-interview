cf068fb9787e09130512a2a8285b95094cc766a2
Benjamin Desole <benjamin.desole@gmail.com>
2022-10-01 17:24:14 +0200

    display deleted movie in success snack bar

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { movies$ } from "./movies";
 import { Container } from "@mui/system";
 import { Grid, Skeleton } from "@mui/material";
 import MovieCard from "./components/MovieCard";
+import SimpleSnackbar from "./components/SnabarBar";
 
 export type movieObject = {
   id?: string;
@@ -13,8 +14,27 @@ export type movieObject = {
   handleMovieDelete?: () => void;
 };
 
+export type snackBarObject = {
+  handleClose: (event: React.SyntheticEvent | Event) => void;
+  open: boolean;
+  message: string;
+  severity: "success" | "error" | "info" | "warning" | undefined;
+};
+
 function App() {
   const [movies, setMovies] = useState<movieObject[] | null>(null);
+  const [snackBar, setSnackBar] = useState<snackBarObject>({
+    handleClose: (event: React.SyntheticEvent | Event, reason?: string) => {
+      if (reason === "clickaway") {
+        return;
+      }
+
+      setSnackBar({ ...snackBar, open: false });
+    },
+    open: false,
+    message: "",
+    severity: undefined,
+  });
 
   useEffect(() => {
     movies$.then((value) => setMovies(value));
@@ -63,6 +83,12 @@ function App() {
       const findedIndexMovie = movies.findIndex(
         (movie) => movie.id === movieId
       );
+      setSnackBar({
+        ...snackBar,
+        open: true,
+        message: `Film ${_movies[findedIndexMovie].title} supprimé !`,
+        severity: "success",
+      });
       _movies.splice(findedIndexMovie, 1);
       setMovies(_movies);
     }
@@ -85,6 +111,12 @@ function App() {
             ))
           : displayLoadingSkeletons()}
       </Grid>
+      <SimpleSnackbar
+        handleClose={snackBar.handleClose}
+        open={snackBar.open}
+        message={snackBar.message}
+        severity={snackBar.severity}
+      />
     </Container>
   );
 }
---
+++ src/components/SnabarBar.tsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+import { Stack, Snackbar } from "@mui/material";
+import MuiAlert, { AlertProps } from "@mui/material/Alert";
+import { snackBarObject } from "../App";
+
+export default function SimpleSnackbar({
+  handleClose,
+  open,
+  message,
+  severity,
+}: snackBarObject) {
+  const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
+    props,
+    ref
+  ) {
+    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+  });
+
+  return (
+    <Stack spacing={2} sx={{ width: "100%" }}>
+      <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity={severity} sx={{ width: "100%" }}>
+          {message}
+        </Alert>
+      </Snackbar>
+    </Stack>
+  );
+}
