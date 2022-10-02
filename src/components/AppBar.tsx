6c99923f25e35629c81a777dcf9016589adea6ef
Benjamin Desole <benjamin.desole@gmail.com>
2022-10-02 21:54:46 +0200

    Update AppBar.tsx

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -1,30 +1,13 @@
 import * as React from "react";
-import { styled, alpha } from "@mui/material/styles";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import MenuIcon from "@mui/icons-material/Menu";
-import SearchIcon from "@mui/icons-material/Search";
-import { InputAdornment, MenuItem, TextField } from "@mui/material";
+import { MenuItem, TextField } from "@mui/material";
 import { MoviesContext } from "../context/AppProvider";
 import { movieObject } from "../types/Types";
-
-const Search = styled("div")(({ theme }) => ({
-  position: "relative",
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  "&:hover": {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginLeft: 0,
-  width: "100%",
-  [theme.breakpoints.up("sm")]: {
-    marginLeft: theme.spacing(1),
-    width: "auto",
-  },
-}));
 
 export default function SearchAppBar() {
   const { movies }: any = React.useContext(MoviesContext);
@@ -71,7 +54,7 @@ export default function SearchAppBar() {
             style={{
               backgroundColor: "white",
               padding: 7,
-              borderRadius: 5
+              borderRadius: 5,
             }}
           >
             <TextField
@@ -80,6 +63,7 @@ export default function SearchAppBar() {
               }}
               select
               label="Catégorie"
+              size="small"
             >
               <MenuItem value={"aucune"}>Aucune</MenuItem>
               {renderCategories()}
